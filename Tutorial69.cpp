eea47cf25fccb2cacdbf165ee290ddcb010954c3
Shubham Samrat <shubhamsamrat382@gmail.com>
2021-07-29 11:30:24 +0530

    STL Tutorials

--- Tutorial69.cpp
+++ Tutorial69.cpp
@@ -12,7 +12,31 @@ HP Company -> Employees: 1. Alexendar 2.Menglee ----> CREDITS
 
 Components of STL:
   1. Containers (is an object which stores data) ex: Vector, uses template classes
-  2. Algorithms
-  3. Iterators
+  2. Algorithms (procedure to process and store data)
+  3. Iterators (object "not pointers" which points to an element of a container)
 
+  Containers are of three types -->
+
+  1. Sequence containers -> vector, List, dequeue {Linear access} ---> all types of iterator are supported
+  2. Associative containers -> set, multiset, map, multimap  {uses BST to store the data, direct access} ----> Only Bidirectional iterator are supported
+  3. Derived Containers -> stack, queues, trees, priority queue {Real World Modelling} ---> No Iterators are supported
+
+
+  STL = Containers + Algo + Iterators
+
+  1. Sequence Containers:
+     1. vector -> Random access ->fast (contigous memory), Random access iterator is supported
+               -> insertion & deletion -> slow (shift all the elements)
+               -> insertion and deletion at the end -> fast
+
+    2. List -> Random access -> slow (memory is not contigous), Bidirectional iterator is supported
+            -> Middle Insertion or deletion -> fast
+            -> Insertion/deletion at end or first -> fast
+
+    3. Deque -> Random access iterator is supported
+
+    To Be Continued........
+      
+  2. Associative containers: {all operations are fast in O(log n) time except random access}
+      
 */
---
+++ Tutorial72.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // List is doubly linked list which has all the propety of Linked list
+    // When we want the insertion and deletion to be fast --> linked list
+    // When we want the access to be fast ---> vector
+    return 0;
+}
